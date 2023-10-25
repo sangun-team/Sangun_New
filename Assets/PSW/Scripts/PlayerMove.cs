8cc06be3a7422046f28542102a0efd7c3f4a8d0c
parkwish <parkthwon@kakao.com>
2023-10-25 10:53:16 +0900

    호랑이 anim 적용

--- Assets/PSW/Scripts/PlayerMove.cs
+++ Assets/PSW/Scripts/PlayerMove.cs
@@ -9,6 +9,14 @@ public class PlayerMove : MonoBehaviour
 
     // 회전 속도
     public float rotateSpeed = 10f;
+
+    // animation
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
     void Update()
     {
         float h = Input.GetAxis("Horizontal");
@@ -25,7 +33,18 @@ public class PlayerMove : MonoBehaviour
             transform.position += dir * speed * Time.deltaTime;
             // 회전하는 부분
             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * rotateSpeed);
+
+            // 애니메이션 설정
+            anim.SetBool("Idle", false);
+            anim.SetBool("Run", true);
         }
+        else if (h == 0 && v == 0)
+        {
+            // 애니메이션 설정
+            anim.SetBool("Run", false);
+            anim.SetBool("Idle", true);
+        }
+
         //Vector3 velocity = dir * speed;
         //transform.position += velocity * Time.deltaTime;
     }
